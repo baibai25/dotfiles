f1a07ad8e963e360c780708f9f87de5a6afb0306
baibai25 <kamuy.camel.0225@gmail.com>
2020-01-04 23:14:07 +0900

    Update

--- link.sh
+++ link.sh
@@ -2,3 +2,5 @@
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/manjaro/i3/config ~/.i3/config
+ln -s ~/dotfiles/manjaro/i3/i3pystatus_config.py ~/.i3/i3pystatus_config.py
