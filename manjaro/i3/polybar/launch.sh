c95a0435d72712f063457c98476bdc52f3d50b8e
baibai25 <kamuy.camel.0225@gmail.com>
2021-05-06 17:40:50 +0900

    Update for KIPP

--- manjaro/i3/polybar/launch.sh
+++ manjaro/i3/polybar/launch.sh
@@ -9,25 +9,33 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Display port
 port_primary=`xrandr --query | grep " primary" | cut -d " " -f1`
 port_nonprimary=`xrandr --query | grep -v " primary" | grep " connected" | cut -d " " -f1`
+port_nonprimaries=(`echo $port_nonprimary`)
+echo $port_primary
+echo $port_nonprimary
 
 # Check display settings (duplicate or not)
 primary=`xrandr --query | grep " primary" | cut -d " " -f4`
 nonprimary=`xrandr --query | grep -v " primary" | grep " connected" | cut -d " " -f3`
+nonprimaries=(`echo $nonprimary`)
+echo $primary
+echo $nonprimary
 
-if [ "$nonprimary" = "" ]; then
-    echo "Single display"
-    MONITOR_primary=$port_nonprimary polybar --reload single &
- 
-elif [ $primary = $nonprimary ]; then
-    echo "Mirror"
-    MONITOR_primary=$port_nonprimary polybar --reload mirror &
+MONITOR_primary=$port_primary polybar --reload single &
+for i in ${!nonprimaries[@]}; do
+    if [ "$nonprimaries[i]" = "" ]; then
+        echo "Single display"
+        MONITOR_primary=$port_nonprimaries[i] polybar --reload single &
+        break
     
-elif [ $primary != $nonprimary ]; then
-    echo "Multiple"
-    MONITOR_primary=$port_primary polybar --reload single &
-    
-    for m in $port_nonprimary; do
-        MONITOR_sub=$m polybar --reload extend &
-    done
-fi
+    elif [ $primary = $nonprimaries[i] ]; then
+        echo "Mirror"
+        MONITOR_primary=$port_nonprimaries[i] polybar --reload mirror &
+        
+    elif [ $primary != ${nonprimaries[i]} ]; then
+        echo "Multiple"
 
+        for m in ${port_nonprimaries[i]}; do
+            MONITOR_sub=$m polybar --reload extend &
+        done
+    fi
+done
